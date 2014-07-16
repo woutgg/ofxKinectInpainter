ea4682ca5839b1282a7f655ce93c30e1c955a2c1
Wouter R <gennerate@zonnet.nl>
2014-07-16 13:31:09 +0200

    Fix indentation.

--- src/ofxKinectInpainter.cpp
+++ src/ofxKinectInpainter.cpp
@@ -13,33 +13,30 @@ void ofxKinectInpainter::setup(int width, int height, int downsampling) {
 	inpainted.allocate(width, height);
 	scaled.allocate((float)width/scale, (float)height/scale);
 	scaledMask.allocate((float)width/scale, (float)height/scale);
-	
 }
 
 
 // inpainting happens at a low resolution becuse it's quite
 // expensive to compute
 void ofxKinectInpainter::inpaint(ofxCvGrayscaleImage &img) {
-
-	
 	// inverted threshold at pix>1 will give a mask of holes only.
 	cvThreshold(img.getCvImage(), mask.getCvImage(), 1, 255, CV_THRESH_BINARY_INV);
 	mask.flagImageChanged();
-	
+
 	// scale the mask down
 	scaledMask.scaleIntoMe(mask);
-	
+
 	// make a scaled version of the image for inpainting
 	scaled.scaleIntoMe(img);
-	
-	
+
+
 	// do the actual inpainting on a low res version
 	// using INPAINT_NS is faster than the other option (cv::INPAINT_TELEA)
 	cv::Mat imgg = scaled.getCvImage();
 	cv::inpaint(scaled.getCvImage(), scaledMask.getCvImage(), imgg, inpaintRadius, cv::INPAINT_NS);
 	scaled.flagImageChanged();
-	
-	
+
+
 	// scale up the inpainted image
 	inpainted.scaleIntoMe(scaled, CV_INTER_LINEAR);
 
@@ -52,6 +49,7 @@ void ofxKinectInpainter::inpaint(ofxCvGrayscaleImage &img) {
 void ofxKinectInpainter::setInpaintingRadius(int radius) {
 	inpaintRadius = radius;
 }
+
 void ofxKinectInpainter::setDownsampling(int downsampling) {
 	if(scale!=downsampling) {
 		scale = downsampling;
@@ -59,7 +57,6 @@ void ofxKinectInpainter::setDownsampling(int downsampling) {
 		scaledMask.allocate((float)width/scale, (float)height/scale);
 	}
 }
-
 
 int ofxKinectInpainter::getInpaintingRadius() {
 	return inpaintRadius;
