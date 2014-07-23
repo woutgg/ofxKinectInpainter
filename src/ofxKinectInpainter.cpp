8ed4b9f6f6a287d425fb52a2b7e0e0e60df9bdbb
Wouter R <gennerate@zonnet.nl>
2014-07-23 18:55:46 +0200

    Update to support ofxCv (Note: the addon might currently be broken...).

--- src/ofxKinectInpainter.cpp
+++ src/ofxKinectInpainter.cpp
@@ -9,15 +9,25 @@ void ofxKinectInpainter::setup(int width, int height, int downsampling) {
 	scale = downsampling;
 	this->width = width;
 	this->height = height;
+
+#ifndef OFX_CV
 	mask.allocate(width, height);
 	inpainted.allocate(width, height);
 	scaled.allocate((float)width/scale, (float)height/scale);
 	scaledMask.allocate((float)width/scale, (float)height/scale);
+#else
+	mask.create(width, height, CV_8UC1);
+	inpainted.create(width, height, CV_8UC1);
+	scaled.create((float)width / scale, (float)height / scale, CV_8UC1);
+	scaledMask.create((float)width / scale, (float)height / scale, CV_8UC1);
+#endif
 }
 
 
 // inpainting happens at a low resolution becuse it's quite
 // expensive to compute
+#ifndef OFX_CV
+
 void ofxKinectInpainter::inpaint(ofxCvGrayscaleImage &img) {
 	// inverted threshold at pix>1 will give a mask of holes only.
 	cvThreshold(img.getCvImage(), mask.getCvImage(), 1, 255, CV_THRESH_BINARY_INV);
@@ -31,7 +41,7 @@ void ofxKinectInpainter::inpaint(ofxCvGrayscaleImage &img) {
 
 
 	// do the actual inpainting on a low res version
-	// using INPAINT_NS is faster than the other option (cv::INPAINT_TELEA)
+	// using INPAINT_NS is faster than the other option (cv::INPAINT_TELEA seems more stable though)
 	cv::Mat imgg = scaled.getCvImage();
 	const cv::Mat maskImg = scaledMask.getCvImage();
 	cv::inpaint(imgg, maskImg, imgg, inpaintRadius, cv::INPAINT_NS);
@@ -47,6 +57,31 @@ void ofxKinectInpainter::inpaint(ofxCvGrayscaleImage &img) {
 	img.flagImageChanged();
 }
 
+#else
+
+void ofxKinectInpainter::inpaint(cv::Mat &img) {
+	cv::threshold(img, mask, 1, 255, CV_THRESH_BINARY_INV);
+
+	// scale the mask down
+	scaleInto(mask, scaledMask);
+
+	// make a scaled version of the image for inpainting
+	scaleInto(img, scaled);
+
+	// do the actual inpainting on a low res version
+	// using INPAINT_NS is faster than the other option (cv::INPAINT_TELEA seems more stable though)
+	cv::inpaint(scaled, scaledMask, scaled, inpaintRadius, cv::INPAINT_NS);
+
+	// scale up the inpainted image
+	scaleInto(scaled, inpainted, cv::INTER_LINEAR);
+
+	//inpainted.convertToRange(0, 100);
+	// now blend it with the orignal, using a mask
+	inpainted.copyTo(img, mask);
+}
+
+#endif
+
 void ofxKinectInpainter::setInpaintingRadius(int radius) {
 	inpaintRadius = radius;
 }
@@ -54,8 +89,13 @@ void ofxKinectInpainter::setInpaintingRadius(int radius) {
 void ofxKinectInpainter::setDownsampling(int downsampling) {
 	if(scale!=downsampling) {
 		scale = downsampling;
+#ifndef OFX_CV
 		scaled.allocate((float)width/scale, (float)height/scale);
 		scaledMask.allocate((float)width/scale, (float)height/scale);
+#else
+		scaled.create((float)width / scale, (float)height / scale, CV_8UC1);
+		scaledMask.create((float)width / scale, (float)height / scale, CV_8UC1);
+#endif
 	}
 }
 
@@ -65,4 +105,27 @@ int ofxKinectInpainter::getInpaintingRadius() {
 
 int ofxKinectInpainter::getDownsampling() {
 	return scale;
+}
+
+/* private */
+void ofxKinectInpainter::scaleInto(cv::Mat &src, cv::Mat &dst, int interp) {
+	cv::Size sz = src.size();
+
+	if (sz.width == 0 || sz.height == 0) {
+		ofLogError("ofxKinectInpainter") << "scaleInto(): source image is empty";
+		return;
+	}
+
+	if (dst.channels() != dst.channels() || src.depth() != dst.depth()) {
+		ofLogError("ofxKinectInpainter") << "scaleInto(): type mismatch with source image";
+		return;
+	}
+
+	if (interp != cv::INTER_NEAREST && interp != cv::INTER_LINEAR &&
+		interp != cv::INTER_AREA && interp != cv::INTER_CUBIC && interp != cv::INTER_LANCZOS4) {
+		ofLogWarning("ofxKinectInpainter") << "scaleInto(): setting interpolationMethod to cv::INTER_NEAREST";
+		interp = cv::INTER_NEAREST;
+	}
+
+	cv::resize(src, dst, dst.size(), 0, 0, interp);
 }
--- src/ofxKinectInpainter.h
+++ src/ofxKinectInpainter.h
@@ -1,6 +1,13 @@
-
+/*
+ * Note: to use with ofxCv instead of ofxOpenCv, add '-DOFX_CV' to 'Other C++ Flags' (under LLVM Custom compiler flags)
+ */
 #pragma once
-#include "ofxCvGrayscaleImage.h"
+
+#ifndef OFX_CV
+# include "ofxCvGrayscaleImage.h"
+#else
+# include "ofxCv.h"
+#endif
 
 class ofxKinectInpainter {
 public:
@@ -20,17 +27,33 @@ public:
 	
 	int getInpaintingRadius();
 	int getDownsampling();
-	
+
+#ifndef OFX_CV
 	void inpaint(ofxCvGrayscaleImage &img);
+#else
+	void inpaint(cv::Mat &img);
+#endif
+
 private:
+#ifndef OFX_CV
 	ofxCvGrayscaleImage scaled;
 	ofxCvGrayscaleImage mask;
 	ofxCvGrayscaleImage scaledMask;
 	
 	ofxCvGrayscaleImage inpainted;
+#else
+	cv::Mat scaled;
+	cv::Mat mask;
+	cv::Mat scaledMask;
+
+	cv::Mat inpainted;
+#endif
+
 	float scale;
 	int width;
 	int height;
 	static const int DEFAULT_INPAINT_RADIUS = 3;
 	int inpaintRadius;
+
+	void scaleInto(cv::Mat &src, cv::Mat &dst, int interpolation = cv::INTER_LINEAR);
 };
